d67394f4aa1b7056b75efca5dc628698c4b997b8
Tobias B <belkner_tobias93@web.de>
2020-06-06 13:26:40 +0200

    Removed Blubb
    Blubb has no sense of existence on that website

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
           <li className="App__navitem"><a href="#">Home</a></li>
           <li className="App__navitem"><a href="#">About</a></li>
           <li className="App__navitem"><a href="#">Project</a></li>
-          <li className="App__navitem"><a href="#">Blubb</a></li>
         </ul>
         <button className="App__button">Contact</button>
       </header>
